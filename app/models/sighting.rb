8e5e0f93dc8207c46d05da2c9d2683c4d959f778
tkellogg <timothy.kellogg@gmail.com>
2013-01-30 19:32:00 -0700

    added test data to gh-pages

--- app/models/sighting.rb
+++
@@ -1,3 +0,0 @@
-class Sighting < ActiveRecord::Base
-  attr_accessible :city, :duration, :match_score, :posted_at, :shape, :sighted_at, :state, :summary, :x_coord, :y_coord
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-UfoSighting::Application.config.secret_token = '0fb30b8fe02fdccca1709fb5e3226a58bd061e702a0673b9b36f64ec9612b3c4710e719ba6ff444214ada00e90f830b17366cd24a3ade3443f5d564271c25321'
