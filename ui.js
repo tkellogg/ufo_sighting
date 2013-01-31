6cdcf9962254e89cde2c165254a3e1e5a07ae303
Tyler Wanlass <twanlass@gmail.com>
2013-01-30 19:58:23 -0700

    Adding map styles and custom UFO icon

--- ui.js
+++ ui.js
@@ -8,6 +8,29 @@ var json = [
 	}
 ];
 
+var styles = [
+  {
+    "featureType": "water",
+    "stylers": [
+      { "color": "#4a6683" },
+      { "lightness": -21 },
+      { "saturation": -73 }
+    ]
+  },{
+    "featureType": "landscape",
+    "stylers": [
+      { "color": "#598662" },
+      { "saturation": -61 }
+    ]
+  },{
+    "featureType": "administrative",
+    "stylers": [
+      { "visibility": "simplified" }
+    ]
+  }
+]
+
+
 function SightingMap(map) {
 	this.map = map;
 };
@@ -19,7 +42,8 @@ SightingMap.prototype.load_data = function (data) {
 			var marker = new google.maps.Marker({
 					position: myLatlng,
 					map: this.map,
-					title: sighting.summary
+					title: sighting.summary,
+					icon: 'https://dl.dropbox.com/u/1031392/ufo_marker.png'
 			});
 		}
 };
@@ -28,7 +52,8 @@ $(function() {
 	var mapOptions = {
 		center: new google.maps.LatLng(32.997286, -87.623444),
 		zoom: 8,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		styles: styles
 	};
 
 	var map = new google.maps.Map(document.getElementById("map_canvas"),
