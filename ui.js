895fb92db2dee66fad854b391efb99cde60bdf7e
tkellogg <timothy.kellogg@gmail.com>
2013-01-30 19:45:06 -0700

    loading our json onto a map and centering

--- ui.js
+++ ui.js
@@ -1,26 +1,41 @@
+var json = [
+	{
+		city: 'Moundville',
+		state: 'AL',
+		date_time: '1/18/03 02:30',
+		coord_lon: -87.623444,
+		coord_lat: 32.997286
+	}
+];
 
+function SightingMap(map) {
+	this.map = map;
+};
+
+SightingMap.prototype.load_data = function (data) {
+		for (var i in data) {
+			var sighting = data[i];
+			var myLatlng = new google.maps.LatLng(sighting.coord_lat, sighting.coord_lon);
+			var marker = new google.maps.Marker({
+					position: myLatlng,
+					map: this.map,
+					title: sighting.summary
+			});
+		}
+};
 
 $(function() {
 	var mapOptions = {
-		center: new google.maps.LatLng(-34.397, 150.644),
+		center: new google.maps.LatLng(32.997286, -87.623444),
 		zoom: 8,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 
 	var map = new google.maps.Map(document.getElementById("map_canvas"),
 			mapOptions);
+	var sightingMap = new SightingMap(map);
 
-	$.getJSON('sightings.json', null, function(data) {
-
-		for (var i in data) {
-			var sighting = data[i];
-			var myLatlng = new google.maps.LatLng(sighting.coord_lat, sighting.coord_lon);
-			var marker = new google.maps.Marker({
-					position: myLatlng,
-					map: map,
-					title: sighting.summary
-			});
-		}
-
-	});
+	var url = 'http://timkellogg.me/ufo_sighting/sightings.json';
+	//$.getJSON(url, null, function(data) { sightingMap.load_data.apply(sightinMap, data); });
+	sightingMap.load_data(json);
 });
