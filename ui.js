23e5699a97233b329be1469186e7fb1e2e52a88d
tkellogg <timothy.kellogg@gmail.com>
2013-01-30 19:28:53 -0700

    Added some test data & structure

--- ui.js
+++ ui.js
@@ -9,4 +9,18 @@ $(function() {
 
 	var map = new google.maps.Map(document.getElementById("map_canvas"),
 			mapOptions);
+
+	$.getJSON('sightings.json', null, function(data) {
+
+		for (var i in data) {
+			var sighting = data[i];
+			var myLatlng = new google.maps.LatLng(sighting.coord_lat, sighting.coord_lon);
+			var marker = new google.maps.Marker({
+					position: myLatlng,
+					map: map,
+					title: sighting.summary
+			});
+		}
+
+	});
 });
